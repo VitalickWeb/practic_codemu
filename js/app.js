8111e694b619559030959422483ae3c8fbda7360
Vitalick Web <miraigeweb@gmail.com>
2024-04-22 17:59:06 +0300

    created the input screen

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const screenKeybord = (function() {
+
+    function init(containerInit) {
+        const container = document.querySelector(containerInit)
+
+        
+
+    }
+
+    return {
+        init
+    }
+})('project_app_screen_keybord')
+
+screenKeybord.init('project_app_screen_keybord')
